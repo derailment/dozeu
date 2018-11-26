33da75aa2566de707cbd9708d0097fba8d03158d
derailment <abby8050@gmail.com>
2018-11-26 15:46:47 +0800

    Finish reroute testing

--- query_test.py
+++ query_test.py
@@ -3,7 +3,12 @@ from config import ONOS_IP, ONOS_PORT
 from utils import json_get_req, json_post_req
 import logging
 
-data = {'paths': []}
+data = {'paths': [
+    {'path': ["00:00:00:00:00:02/None", "of:0000000000000001", "of:0000000000000002", "00:00:00:00:00:04/None"]},
+    {'path': ["00:00:00:00:00:04/None", "of:0000000000000002", "of:0000000000000001","00:00:00:00:00:02/None"]}
+  ]
+}
+
 reply = json_post_req('http://%s:%d/reroute' % (ONOS_IP, ONOS_PORT), json.dumps(data))
 print reply
 
