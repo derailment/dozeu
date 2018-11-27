10af9db0426ff95941e152abcc4fd31f919e06fc
derailment <abby8050@gmail.com>
2018-11-27 11:20:58 +0800

    Update REST API test

--- query_test.py
+++ query_test.py
@@ -12,8 +12,8 @@ data = {'paths': [
 reply = json_post_req('http://%s:%d/reroute' % (ONOS_IP, ONOS_PORT), json.dumps(data))
 print reply
 
-reply = json_get_req('http://%s:%d/topology' % (ONOS_IP, ONOS_PORT))
+reply = json_get_req('http://%s:%d/bandwidth/topology' % (ONOS_IP, ONOS_PORT))
 print reply
 
-reply = json_get_req('http://%s:%d/bandwidth' % (ONOS_IP, ONOS_PORT))
+reply = json_get_req('http://%s:%d/bandwidth/connections' % (ONOS_IP, ONOS_PORT))
 print reply
